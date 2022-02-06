d34787a9f223848fe7f7f5cf295408fbd15e973a
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2022-02-06 23:11:40 +0530

    dsahboard and routines file added

---
+++ lib/Patient/Dashboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class UserDashboard extends StatefulWidget {
+  const UserDashboard({Key? key}) : super(key: key);
+
+  @override
+  _UserDashboardState createState() => _UserDashboardState();
+}
+
+class _UserDashboardState extends State<UserDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
