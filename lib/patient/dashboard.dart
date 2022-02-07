af72db8251392182aaf9d8bb90b1e5494d7d6450
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2022-02-07 19:25:31 +0530

    dashboard

--- lib/patient/dashboard.dart
+++ lib/patient/dashboard.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:flip_card/flip_card.dart';
 
 class UserDashboard extends StatefulWidget {
   const UserDashboard({Key? key}) : super(key: key);
@@ -9,8 +9,225 @@ class UserDashboard extends StatefulWidget {
 }
 
 class _UserDashboardState extends State<UserDashboard> {
+  _renderBg() {
+    return Container(
+      decoration: const BoxDecoration(color: Color(0xFFFFFFFF)),
+    );
+  }
+
+  _renderAppBar(context) {
+    return MediaQuery.removePadding(
+      context: context,
+      removeBottom: true,
+      child: AppBar(
+        elevation: 0.0,
+        backgroundColor: const Color(0x00FFFFFF),
+      ),
+    );
+  }
+
+  _renderContent(context) {
+    return Card(
+      elevation: 0.0,
+      margin: const EdgeInsets.only(
+          left: 32.0, right: 32.0, top: 0.0, bottom: 10.0),
+      color: const Color(0x00000000),
+      child: FlipCard(
+        direction: FlipDirection.HORIZONTAL,
+        speed: 1000,
+        onFlipDone: (status) {
+          print(status);
+        },
+        front: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF006666),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Upcoming Session',
+                  style: Theme.of(context).textTheme.headline4),
+              Text('Click to know date',
+                  style: Theme.of(context).textTheme.bodyText1),
+            ],
+          ),
+        ),
+        back: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF006666),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Date', style: Theme.of(context).textTheme.headline1),
+              Text('Click to flip',
+                  style: Theme.of(context).textTheme.bodyText1),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          _renderBg(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _renderAppBar(context),
+              Expanded(
+                flex: 4,
+                child: Card(
+                  elevation: 0.0,
+                  margin: const EdgeInsets.only(
+                      left: 32.0, right: 32.0, top: 0.0, bottom: 10.0),
+                  color: const Color(0x00000000),
+                  child: FlipCard(
+                    direction: FlipDirection.HORIZONTAL,
+                    speed: 1000,
+                    onFlipDone: (status) {
+                      print(status);
+                    },
+                    front: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF006666),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Total Sessions',
+                              style: Theme.of(context).textTheme.headline4),
+                          Text('Click to know',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                    back: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF006666),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Sessions',
+                              style: Theme.of(context).textTheme.headline3),
+                          Text('Click to flip',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Card(
+                  elevation: 0.0,
+                  margin: const EdgeInsets.only(
+                      left: 32.0, right: 32.0, top: 0.0, bottom: 10.0),
+                  color: const Color(0x00000000),
+                  child: FlipCard(
+                    direction: FlipDirection.HORIZONTAL,
+                    speed: 1000,
+                    onFlipDone: (status) {
+                      print(status);
+                    },
+                    front: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF006666),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Upcoming Session',
+                              style: Theme.of(context).textTheme.headline4),
+                          Text('Click to know date',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                    back: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF006666),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Date',
+                              style: Theme.of(context).textTheme.headline3),
+                          Text('Click to flip',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Card(
+                  elevation: 0.0,
+                  margin: const EdgeInsets.only(
+                      left: 32.0, right: 32.0, top: 0.0, bottom: 10.0),
+                  color: const Color(0x00000000),
+                  child: FlipCard(
+                    direction: FlipDirection.HORIZONTAL,
+                    speed: 1000,
+                    onFlipDone: (status) {
+                      print(status);
+                    },
+                    front: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF006666),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Previous Session',
+                              style: Theme.of(context).textTheme.headline4),
+                          Text('Click to know date',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                    back: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF006666),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Date',
+                              style: Theme.of(context).textTheme.headline3),
+                          Text('Click to flip',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
